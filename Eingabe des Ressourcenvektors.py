bda7e055bc95b95591abe24887c5a44d3bf6cc1f
latashossain <latashossain@gmail.com>
2024-06-18 20:53:06 +0200

    Eingabe des Ressourcenvektors und Funktion zur Deadlock-Erkennung
    Eingabe des Ressourcenvektors; der Code ist fertig und muss nicht geändert werden.
    
    Funktion zur Deadlock-Erkennung: noch anpassen, Fehler korrigieren und übersichtlicher machen

--- Eingabe des Ressourcenvektors.py
+++
@@ -1,7 +0,0 @@
-#Eingabe des Ressourcenvektors
-ressoucen_laenge = int(input("Geben Sie die Anzahl der Ressourcen ein: "))
-
-ressourcentypen = []
-for i in range(ressoucen_laenge):
-    eingabe= int(input("Geben Sie die Ressource "+str(i+1)+" ein"))
-    ressourcentypen.append(eingabe)
--- MatrixDimension.py
+++ MatrixDimension.py
@@ -1,3 +1,10 @@
+#Eingabe des Ressourcenvektors
+ressourcen_laenge = int(input("Geben Sie die Anzahl der Ressourcen ein: "))
+ressourcentypen = []
+for i in range (ressourcen_laenge):
+    eingabe = int(input("Geben Sie dir Ressource "+str(i+1)+" ein: "))
+    ressourcentypen.append(eingabe)
+
 
 def matrix_dimension():
     print("Geben Sie die Dimension der Matrix ein: ")
@@ -20,4 +27,32 @@ print("Die eingegebene Matrix ist: ")
 for zeile in matrix:
     print(zeile)
 
+
+
+#Funktion zur Deadlock-Erkennung
+def is_deadlock (ressourcentypen, matrix): #anforderungsmatrix fehlt noch
+    #Initialisierung von:
+    work = ressourcentypen[:]
+    request = [5,7,4]
+             [2,3,4] #Eingabe code und Datei wird noch gemacht
+    
+    finish = [False]*len(ressourcentypen)
+
+    for i in range (len(matrix)):
+      if request [i][j] == 0 for j in range(len(ressourcentypen)):
+          finish [i] = True
+      else:
+          finish[i]= False
+
+     #Index i suchen :
+    for i in range (len(matrix)):
+        if not finish[i] and all(request[i][j]<= work[j]for j in range(len(ressourcentypen))):
+            for j in range (len(ressourcentypen)):
+                work[j] += matrix[i][j]
+            finish [i] = True
+
+    if all(finish):
+        print("Keinen Deadlock erkannt.")
+    else:
+        print("Deadlock erkannt.")
 

c3e4b144a48628259d7dd03e74235a6a04cc4b64
bbA9993 <basma.akh15@gmail.com>
2024-06-21 03:14:48 +0200

    Update datei einlesen code.py

--- datei einlesen code.py
+++ datei einlesen code.py
@@ -1,37 +1,47 @@
 import os
 import argparse
+import sys
 
 
 def datei_überprüfung(dateipfad):
     # datei_überprüfung ist die Methode bzw. Argument
     # dateipfad ist das Argument, welches angenommen wird
+    if not os.path.isfile(dateipfad):
+        print("Datei existiert nicht!")
+        sys.exit(1)
+        #sys.exit(1) dient dazu um das Programm zu beenden
 
-    if __name__ == "__main__":
-        parser = argparse.ArgumentParser(description="Datei zum Einlesen des Ressourcenvektors, Belegungsmatrix und Anforderungsmatrix.")
-        parser.add_argument('-ressourcenvektor', type=str, help='Datei für Ressourcenvektor')
-        parser.add_argument('-belegungsmatrix', type=str, help='Datei für Belegungsmatrix')
-        parser.add_argument('-anforderungsmatrix', type=str, help='Datei für Anforderungsmatrix')
-        # str, weil der Wert des Arguments als Zeichenkette behandelt wird, da ein Dateiname eine Zeichenkette ist
-        args = parser.parse_args()
-        # damit das Programm die Informationen versteht und verarbeiten kann
+def read_vector(file_path):
+   datei_überprüfung(file_path)
+    #try....
 
-        ressourcenvektor = read_vector(args.ressourcenvektor)
-        belegungsmatrix = read_matrix(args.belegungsmatrix)
-        anforderungsmatrix = read_matrix(args.anforderungsmatrix)
-        
-def read_vector(filepath):
-    matrix = []
+        with open(filepath, 'r') as file:
+         # 'r' steht nur für das Lesen der Datei
+            vector = list(map(int, file.readline().strip().split()))
+        return vector
+        # return vector, damit der vector zur weiterverarbeitung der deadlock Erkennung verwendet werden kann
 
-    with open(filepath, 'r') as file:
-    #'r' steht nur für das Lesen der Datei
-        for line in file:
-        #Schleife, um jede Zeile der Datei einzulesen
-            line.strip().split()
-            #line.strip() entfernt Leerzeichen
-            matrix.append([float(num) for num in line.strip().split()])
-            #float??
+     # except valueError:....
+
+def read_matrix(file_path):
+    datei_überprüfung(file_path)
+    with open(file_path,'r') as file:
+        matrix = [list(map(int,line.strip().split()))for line in file]
     return matrix
 
-def read_matrix(filepath):
-    return read_vector(filepath)
+def main():
+    parser = argparse.ArgumentParser(description="Datei zum Einlesen des Ressourcenvektors, Belegungsmatrix und Anforderungsmatrix.")
+    parser.add_argument('-ressourcenvektor', type=str, help='Datei für Ressourcenvektor')
+    parser.add_argument('-belegungsmatrix', type=str, help='Datei für Belegungsmatrix')
+    parser.add_argument('-anforderungsmatrix', type=str, help='Datei für Anforderungsmatrix')
+    # str, weil der Wert des Arguments als Zeichenkette behandelt wird, da ein Dateiname eine Zeichenkette ist
+    args = parser.parse_args()
+    # damit das Programm die Informationen versteht und verarbeiten kann
+
+    ressourcenvektor = read_vector(args.ressourcenvektor)
+    belegungsmatrix = read_matrix(args.belegungsmatrix)
+    anforderungsmatrix = read_matrix(args.anforderungsmatrix)
+        
+
+
 
